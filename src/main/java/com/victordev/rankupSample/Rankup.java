80c8de7ea641ceaa03712a1c0fbd2fafd1784c07
Victor <victorgabrielus1831043@gmail.com>
2025-08-11 10:35:33 -0300

    Rankup

---
+++ src/main/java/com/victordev/rankupSample/Rankup.java
@@ -0,0 +1,17 @@
+package com.victordev.rankupSample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rankup extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
